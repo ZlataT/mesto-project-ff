b19fee51cd5e2e8c27732222a600dc235a1d83b3
Zlata <ztovstogan@yandex.ru>
2024-12-10 14:04:54 +0300

    Первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
+const page = document.querySelector('.page'); //все элементы body 
+const placesList = page.querySelector('.places__list'); //элемент куда будем вставлять карточки
+// @todo: DOM узлы
+const deleteButton = document.querySelector('.card__delete-button');
+
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
-
 // @todo: Функция создания карточки
+function createCard (name, link){
+  const cardTemplate = document.querySelector('#card-template').content;
+  let cardElemet = cardTemplate.querySelector('.card').cloneNode(true);//клонирование шаблона карточки
+  
+  cardElemet.querySelector('.card__image').src = link; //запись в значение ссылки картинки
+  cardElemet.querySelector('.card__title').textContent = name; //запись в значение заголовка карточки
+  cardElemet.querySelector('.card__delete-button').addEventListener('click', deleteCard); //добавление кнопке функцию удаления при нажатии
+
+  return cardElemet; // возвращение шаблона подготовленного к заполнению
+}
+
 
 // @todo: Функция удаления карточки
+function deleteCard (evt){
+  const card = evt.target.closest('.card'); // запись события в переменную и поиск предка с определенным классом от нажатой кнопки 
+  card.remove(); //удаление (родителя) карточки
+}
+
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(element){
+  const cardElement = createCard(element.name, element.link)
+  placesList.append(cardElement);
+})
