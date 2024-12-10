12c237c24ce4b5f2e2f1e2c796b4891bf5fb542f
Zlata <ztovstogan@yandex.ru>
2024-12-10 18:33:04 +0300

    Вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,38 @@
 const page = document.querySelector('.page'); //все элементы body 
-const placesList = page.querySelector('.places__list'); //элемент куда будем вставлять карточки
+const cardsContainer = page.querySelector('.places__list'); //элемент куда будем вставлять карточки
+
 // @todo: DOM узлы
-const deleteButton = document.querySelector('.card__delete-button');
 
 // @todo: Темплейт карточки
 
 // @todo: Функция создания карточки
 function createCard (name, link){
   const cardTemplate = document.querySelector('#card-template').content;
-  let cardElemet = cardTemplate.querySelector('.card').cloneNode(true);//клонирование шаблона карточки
+  const cardElemet = cardTemplate.querySelector('.card').cloneNode(true);//клонирование шаблона карточки
   
-  cardElemet.querySelector('.card__image').src = link; //запись в значение ссылки картинки
+  cardElemet.querySelector('.card__image').src = link;
+  cardElemet.querySelector('.card__image').alt = name; //запись в значение ссылки картинки
   cardElemet.querySelector('.card__title').textContent = name; //запись в значение заголовка карточки
-  cardElemet.querySelector('.card__delete-button').addEventListener('click', deleteCard); //добавление кнопке функцию удаления при нажатии
+  cardElemet.querySelector('.card__delete-button').addEventListener('click', function(evt){
+    const card = evt.target.closest('.card'); // запись события в переменную и поиск предка с определенным классом от нажатой кнопки 
+    card.remove(); //удаление (родителя) карточки
+  }); //добавление кнопке функцию удаления при нажатии
 
   return cardElemet; // возвращение шаблона подготовленного к заполнению
 }
 
-
 // @todo: Функция удаления карточки
-function deleteCard (evt){
-  const card = evt.target.closest('.card'); // запись события в переменную и поиск предка с определенным классом от нажатой кнопки 
-  card.remove(); //удаление (родителя) карточки
-}
 
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function(element){
   const cardElement = createCard(element.name, element.link)
-  placesList.append(cardElement);
+  cardsContainer.append(cardElement);
 })
+
+//Большое спасибо за ревью
+//Не поняла немного замечания предать удаление карточки как параметр в функции создания карточки, решила реализовать "одной строкой", но придерживаюсь метода подробного написания кода для более удобной читаемости в дальнейшем
+//Объясните пожалуйста в чем разница вызова функции или передача ее как параметра
+//Не понятно написано замечание в 9 строке
+//"Функция должна принимать как параметры кроме объекта с атрибутами карточки, обработчик события удаления карточки deleteCard."
